8ebe293dedcae7a736390f118ef19fb7bf8941e3
BrandonComins <brandcomin@gmail.com>
2019-05-14 00:31:17 -0700

    reduce delay time to 3 seconds between each possible button press

--- ArduinoPiano/Button_Piano/Button_Piano.ino
+++ ArduinoPiano/Button_Piano/Button_Piano.ino
@@ -6,27 +6,27 @@ void setup() {
 void loop() {
   if (analogRead(A0) < 800){
     Serial.println("0");
-    delay(5000);
+    delay(3000);
   }
   else if (analogRead(A1) < 800)
   {
     Serial.println("1");
-    delay(5000);
+    delay(3000);
   }
   else if (analogRead(A2) < 800)
   {
     Serial.println("2");
-    delay(5000);
+    delay(3000);
   }
   else if (analogRead(A3) < 800)
   {
     Serial.println("3");
-    delay(5000);
+    delay(3000);
   }
   else if (analogRead(A4) < 800)
   {
     Serial.println("4");
-    delay(5000);
+    delay(3000);
   }
   else if (analogRead(A5) < 900)
   {
